183fb743adce895089c6837861fbace6cd119523
jwy10 <1998dnjsdud@gmail.com>
2024-01-22 14:04:52 +0900

    챌린지 페이지 develop

---
+++ js/challenge.js
@@ -0,0 +1,18 @@
+// 챌린지 상단 ; 내가 신청한 챌린지
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+});
